d0ab9125ebc70ec2c639346af82ceb2b97accc7b
James <jfmatthews90@gmail.com>
2017-09-22 19:07:08 -0400

    Updated layouts and views.

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register Post do
-	permit_params :title, :body, :image
+	permit_params :title, :body, :link, :image
 
 	show do |t|
 		attributes_table do
 			row :title
 			row :body
+			row :link
 			row :image do
 				post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "No photo yet")
 			end
@@ -15,6 +16,7 @@ ActiveAdmin.register Post do
 		f.inputs do
 			f.input :title
 			f.input :body
+			f.input :link
 			f.input :image, hint: f.post.image? ? image_tag(post.image.url, height: '100') : content_tag(:span, "Upload JPG/PNG/GIF image")
 		end
 		f.actions
